335b084d7069f7667acbae8abe311076e18ebe68
Lucas <lucasdettmann8@gmail.com>
2024-10-28 21:12:21 -0300

    pesquisa em largura

--- pesquisaDeLargura/pesquisa.py
+++ pesquisaDeLargura/pesquisa.py
@@ -6,23 +6,62 @@ grafo["ana"] = ["lucas","pedro","marcos"]
 
 grafo["luiz"]= ["lucas","martins","helena"]
 
+grafo["pedro"] = ["lucas", "marcos"]
+
+grafo["helena"] = ["lucas", "martins"]
+
+grafo["martins"] = ["lucas","helena"]
+
+grafo["marcos"] = ["pedro", "lucas"]
+
+grafo["henrique"] = ["ana", "luiz"]
+
+
+
 print(grafo)
 
 
-verificados = []
 
-fila = []
 
-def pesquisa():
+def pesquisa(pessoa):
 
-    fila.append(grafo["lucas"])
+    verificados = []
+
+    fila = []
+
+    caminho = ["lucas"]
+
+    fila += grafo["lucas"]
     verificados.append("lucas")
 
     while fila:
+        
+        for pesG in fila:
 
-        if 
+            if pesG == pessoa:
+                
+                caminho.append(pesG)
+                return pesG
+                break
+
+            else:
+                if pesG not in verificados:
+                    verificados.append(pesG)
+                    fila.pop(0)
+                    fila += grafo[pesG]
+
+                else:
+                    fila.remove(pesG)
+                    continue
+
+    else:
+        return "esse nome não esta na vizinhaça"
 
 
+
+        
+
+print(pesquisa("marcos"))
 
 
 
